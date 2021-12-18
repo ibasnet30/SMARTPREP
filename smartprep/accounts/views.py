49ed85e5e1614263ad260364a39880fbcc80bf95
ibasnet30 <ishanbasnet100@gmail.com>
2021-12-18 12:09:55 +0545

    2nd commit

--- smartprep/accounts/views.py
+++ smartprep/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'accounts/homepage.html')
+
+def loginpage(request):
+    return render(request,'accounts/login.html')
--- smartprep/smartprep/urls.py
+++ smartprep/smartprep/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('materials/', include('smartprep.urls')),
+    path('materials/', include('materials.urls')),
     path('admins/', include('admins.urls')),
     path('', include('accounts.urls'))
 
