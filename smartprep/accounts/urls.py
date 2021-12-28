eaef7f53ce2c84bb78562d5e70651fc328dedf0e
sanju133 <sanjumaharjan683@gmail.com>
2021-12-28 11:33:04 +0545

    sign in/up pages

--- smartprep/accounts/urls.py
+++ smartprep/accounts/urls.py
@@ -3,7 +3,10 @@ from .import views
 
 urlpatterns=[
     path('', views.homepage),
-    path('LoginForm', views.loginpage)
+    path('contact/', views.contact),
+    path('about/', views.about),
+    path('LoginForm/', views.loginpage),
+    path('RegisterForm/', views.Registerpage)
 
 
 ]
--- smartprep/accounts/views.py
+++ smartprep/accounts/views.py
@@ -4,5 +4,22 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, 'accounts/homepage.html')
 
+def contact(request):
+    context = {
+        'activate_contact': 'active',
+
+    }
+    return render(request, 'accounts/contact.html',context)
+
+def about(request):
+    context = {
+        'activate_about': 'active',
+
+    }
+    return render(request, 'accounts/aboutus.html',context)
+
 def loginpage(request):
     return render(request,'accounts/login.html')
+
+def Registerpage(request):
+    return render(request,'accounts/register.html')
