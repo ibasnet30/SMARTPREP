a06ac232d18bd93ac316aab11f82f63c5a218bde
sanju133 <sanjumaharjan683@gmail.com>
2021-12-30 15:36:25 +0545

    san

--- smartprep/accounts/urls.py
+++ smartprep/accounts/urls.py
@@ -9,11 +9,7 @@ urlpatterns=[
     path('LoginForm/', views.loginpage),
     path('RegisterForm/', views.Registerpage),
 
-    path('LoginForm', views.loginpage),
-    path('register_page', views.register_user),
 
-    path('login_page', views.login_page),
-    path('logout', views.logout_user)
 
 
 
